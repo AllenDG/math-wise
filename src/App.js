4835e215545e1e5e6dda005cbfaac95586a2690c
AllenDG <allenwalterfiestada@gmail.com>
2023-08-29 03:52:11 +0800

    added sidebar and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import "./style/index.css";
+
+import Home from './pages/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	const [navVisible, showNavbar] = useState(false);
+
+	return (
+		<BrowserRouter >
+			<div className="App">
+				<Navbar visible={ navVisible } show={ showNavbar } />
+				<Routes>
+					<Route path="/" element={<Navigate to="/home" />} />
+					<Route path='/home' element={ <Home/>} />
+					
+					<Route path='/analytics' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Analystics</h1>
+						</div>
+					}/>
+					<Route path='/orders' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Orders</h1>
+						</div>
+					}/>
+					<Route path='/settings' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Settings</h1>
+						</div>
+					}/>
+				</Routes>
+			</div>
+		</BrowserRouter>
   );
 }
 
